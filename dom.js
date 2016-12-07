cf37207d8696d5afc3cbd740a4d2de8ae37ce111
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-06 17:25:07 -0800

    Notes are floating"
    qqq
    q
    
    dnsfj

--- dom.js
+++ dom.js
@@ -5,19 +5,32 @@ console.log("Come back! I love you!");
 var body = document.querySelector('body'); //gets body
 var divMain = document.querySelector('#main'); //gets div "main"
 
+// create the Counter
 var counter = document.createElement('div'); //create div "counter"
 var counterText = counter.textContent = "Total Number of Notes";
+var counterSpan = document.createElement('span');
+counter.classList.add('counter');
+counterSpan.textContent = 0;
+counter.appendChild(counterSpan);
 
+// create the Button
 var button = document.createElement('button'); //create button
 var buttonText = button.textContent = "Add a Note"
+divMain.appendChild(button);
 
+// create Div Note Container
+var noteContainer = document.createElement('div');
+noteContainer.classList.add('note-container');
+body.appendChild(noteContainer);
 
+// creates New Notes on 'click'
 var handleButton = function(event) { //button handler
   console.log('clicked button');
   var div = document.createElement('div');
   var divNote = div.classList.add('note');
-  body.appendChild(div);
+  noteContainer.appendChild(div);
   var span = document.createElement('span');
+  span.classList.add('remove')
   span.textContent = 'X';
   var pTag = document.createElement('p');
   pTag.textContent = 'stuff'
@@ -26,7 +39,7 @@ var handleButton = function(event) { //button handler
 }
 
 divMain.appendChild(counter);
-document.body.appendChild(button);
+divMain.appendChild(button);
 // event listener for button clicked
 button.addEventListener('click', handleButton);
 
