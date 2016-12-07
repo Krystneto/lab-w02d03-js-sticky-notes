22c4c66238bb237a7dff95cd5d9172a695f0e494
Kristopher Parrillo <kkparrillo@gmail.com>
2016-12-06 18:41:19 -0800

    time stamp

--- dom.js
+++ dom.js
@@ -1,20 +1,20 @@
 console.log("Come back! I love you!");
  // JavaScript goes here
 
-// create global vars
+// create global selectors
 var body = document.querySelector('body'); //gets body
 var divMain = document.querySelector('#main'); //gets div "main"
 
 // create the Counter
 var counter = document.createElement('div'); //create div "counter"
-var counterText = counter.textContent = "Total Number of Notes";
+var counterText = counter.textContent = "Total Number of Notes ";
 var counterSpan = document.createElement('span');
 counter.classList.add('counter');
-counterSpan.textContent = null;
+counterSpan.textContent = 0;
 counter.appendChild(counterSpan);
 divMain.appendChild(counter);
 
-// create the Button
+// create the Button to add Note
 var button = document.createElement('button'); //create button
 var buttonText = button.textContent = "Add a Note"
 divMain.appendChild(button);
@@ -25,17 +25,16 @@ noteContainer.classList.add('note-container');
 body.appendChild(noteContainer);
 divMain.appendChild(button);
 
-
-
 // creates New Notes on 'click'
 var handleButton = function(event) { //button handler
   console.log('clicked button');
   var div = document.createElement('div');
   var divNote = div.classList.add('note');
   noteContainer.appendChild(div);
+  var currentDate = new Date();
   var span = document.createElement('span');
   span.classList.add('remove')
-  span.textContent = 'X';
+  span.textContent = 'X ' + currentDate;
   var pTag = document.createElement('p');
   pTag.textContent = '';
   div.appendChild(span);
@@ -44,17 +43,19 @@ var handleButton = function(event) { //button handler
   span.addEventListener('click', handleRemoveNote);
   var noteItems = document.querySelectorAll('.note');
   counterSpan.textContent = " " + noteItems.length;
+  // create variable for Time Stamp
+
+}
+
+// handle to remove the Note and update Counter
+var handleRemoveNote = function(event) {
+  event.target.parentNode.remove();
+  var noteItems = document.querySelectorAll('.note');
+  counterSpan.textContent = " " + noteItems.length
 }
 
 // event listener for button clicked
 button.addEventListener('click', handleButton);
-
-// handle to remove the Note
-var handleRemoveNote = function(event) {
-console.log(event.target);
-  event.target.parentNode.remove();
-}
-
 
 
 
